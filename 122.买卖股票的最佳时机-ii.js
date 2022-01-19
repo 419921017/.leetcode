4589c9b3650413be284ace2346062be32050e72b
power_840 <liuyu19921017@gmail.com>
2022-01-19 13:53:42 +0800

    fix 122

--- 122.买卖股票的最佳时机-ii.js
+++ 122.买卖股票的最佳时机-ii.js
@@ -14,8 +14,22 @@ var maxProfit = function (prices) {
   for (let i = 1; i < prices.length; i++) {
     dp[i] = [0, 0];
     dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-    dp[i][1] = Math.max(dp[i - 1][i], dp[i - 1][0] + prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
   }
   return dp[prices.length - 1][0];
 };
+
+const maxProfit1 = (prices) => {
+  const dp = [[0, -prices[0]]];
+  for (let i = 1; i < prices.length; i++) {
+    dp[i] = [0, 0];
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+  }
+  return dp[prices.length - 1][0];
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit1([7, 1, 5, 3, 6, 4]));
+
 // @lc code=end
